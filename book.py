9f88b15da780abe7d939a12654fed3a61bffd974
jiangxingzhi047-crypto <jiangxingzhi047@gmail.com>
2026-02-01 15:16:18 +0800

    Update book.py

--- book.py
+++ book.py
@@ -1,1 +1,20 @@
+class Book:
+  def __init__(self,title:str,author:str):
+    self.title = title
+    self.author = author
+    self.is_borrowed = False
+    self.borrower = None
 
+  def borrow(self,reader) ->bool:
+    if self.is_borrowed:
+      return False
+    self.is_borrowed = True
+    self.borrower = reader
+    return True
+
+  def return_book(self) -> bool:
+    if not self.is_borrowed :
+      return False
+    self.is_borrowed = False
+    self.borrower = None
+    return True
