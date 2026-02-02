1c0b1956540ffca9504b04b99daccf0da2c6e383
jiangxingzhi047-crypto <jiangxingzhi047@gmail.com>
2026-02-02 10:49:16 +0800

    Add borrow_book method to handle book borrowing
    Implement borrow_book method for borrowing logic.

--- library/library.py
+++ library/library.py
@@ -1,1 +1,21 @@
+from .exceptions import *
 
+
+def borrow_book(self, reader_id, book_title):
+    if reader_id not in self.readers:
+        raise ReaderNotFoundError()
+
+    if book_title not in self.books:
+        raise BookNotFoundError()
+
+    reader = self.readers[reader_id]
+    book = self.books[book_title]
+
+    if not reader.can_borrow():
+        raise BorrowLimitExceededError()
+
+    if book.is_borrowed:
+        raise BookAlreadyBorrowedError()
+
+    book.borrow(reader)
+    reader.add_book(book)
