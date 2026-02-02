6a12e401cb268df896c7d1406ad1f0c968accc48
jiangxingzhi047-crypto <jiangxingzhi047@gmail.com>
2026-02-02 10:48:31 +0800

    Update exceptions.py

--- library/exceptions.py
+++ library/exceptions.py
@@ -1,1 +1,18 @@
+class LibraryError(Exception):
+    pass
 
+
+class BookNotFoundError(LibraryError):
+    pass
+
+
+class ReaderNotFoundError(LibraryError):
+    pass
+
+
+class BookAlreadyBorrowedError(LibraryError):
+    pass
+
+
+class BorrowLimitExceededError(LibraryError):
+    pass
